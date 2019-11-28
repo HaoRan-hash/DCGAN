a28f1ca49193cb0c9e85fab779ba1304ca28515b
HaoRan-hash <54461374+HaoRan-hash@users.noreply.github.com>
2019-11-28 18:21:10 +0800

    Add files via upload

---
+++ extra/handle_images.py
@@ -0,0 +1,12 @@
+import glob
+from PIL import Image
+
+
+if __name__ == "__main__":
+    i = 1
+    for image in glob.glob("D:/BaiduNetdiskDownload/celeba/*"):    #要处理的图片存放的位置
+        picture = Image.open(image)
+
+        new_picture = picture.resize((128, 160))   #图片修改的尺寸，可以根据自己需要进行调整
+        new_picture.save("D:/BaiduNetdiskDownload/celeba/%s.png" % i)   #修改过的图片保存的位置
+        i = i + 1
